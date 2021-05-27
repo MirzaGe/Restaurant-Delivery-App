f731a4c989b06c2fe346aaef020a235211d09b43
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-27 07:00:48 +0500

    commit red

---
+++ Restaurant-Delivery-App/OnboardingViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Restaurant-Delivery-App
+//
+//  Created by sherry on 27/05/2021.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+
+private func setupCollectionView() {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .vertical
+    collectionView.collectionViewLayout = layout
+    
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    
+    
+}
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+        
+    }
+    
+    
+}
